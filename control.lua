47d36b316067c0e3d8b41ceb1f4d216bccd70477
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-01-21 00:03:36 -0700

    Make better-chat optional

--- control.lua
+++ control.lua
@@ -1,5 +1,23 @@
 local shortcodes = require("assets.shortcodes")
+
 script.on_init(function ()
-	remote.call("emojipack registration", "add", script.mod_name, shortcodes)
-	shortcodes = nil
+	if script.active_mods["better-chat"] then
+		global["better-chat"] = true
+		remote.call("emojipack registration", "add", script.mod_name, shortcodes)
+	else
+	end
+	shortcodes = nil -- unnecessary memory ussage
+end)
+script.on_load(function ()
+	script.on_nth_tick(5, function (p1)
+		-- Register with better-chat if it was activated after twemoji
+		-- And mark it as unregistered if better-chat no longer exists
+		if script.active_mods["better-chat"] and not global["better-chat"] then
+			remote.call("emojipack registration", "add", script.mod_name, shortcodes)
+		elseif not script.active_mods["better-chat"] and global["better-chat"] then
+			global["better-chat"] = nil
+		end
+		script.on_nth_tick() -- Unregister
+		shortcodes = nil -- unnecessary memory ussage
+	end)
 end)
