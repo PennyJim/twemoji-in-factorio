eb0db1926b07958742efb3a811a4561853d508dd
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-01-22 20:38:41 -0700

    improve and fix processing
    I was only including the 0's before if it had leading 0's.
    I also added an attempt at checking if twemoji just dropped the fe0f and did indeed have it

--- data-preprocessing/ordering.lua
+++ data-preprocessing/ordering.lua
@@ -44,16 +44,23 @@ for line in io.lines("./data-preprocessing/emoji-ordering.txt") do
 
 	-- Remove leading zeros
 	local _,leadingZero = code:find("^0+")
-	if leadingZero then code = code:sub(1, leadingZero) end
+	if leadingZero then code = code:sub(leadingZero+1) end
 
+	-- Try removing fe0f if it doesn't initially exist
 	local exists = file_exists("./assets/twemoji-images/"..code..".png")
+	if not exists then
+		code = code:gsub("-fe0f", "")
+		exists = file_exists("./assets/twemoji-images/"..code..".png")
+	end
+
 	-- Only add it if twemoji has an image for it
 	if exists then
 		if unregistered_emoji[code] then unregistered_emoji[code] = nil
 		else print(code.." exists twice??") end
 
 		-- Extract and capitalize the name from "# <emoji> emoji name here"
-		local name = split(line, "#")[2]
+		local name_start = line:find("%#")
+		local name = line:sub(name_start)
 		local nameIndex = name:find(" ", 2)
 		name = capitalizeWords(name:sub(nameIndex+1))
 
@@ -66,8 +73,6 @@ end
 --#region -- HACK: Lazily add the ones that don't work properly
 missing_codes = {
 	-- Missing in emoji-ordering.txt
-	{"a9", "Copyright Sign"},
-	{"ae", "Registered Sign"},
 	{"1f1e6", "Regional Indicator A"},
 	{"1f1e7", "Regional Indicator B"},
 	{"1f1e8", "Regional Indicator C"},
@@ -94,19 +99,6 @@ missing_codes = {
 	{"1f1fd", "Regional Indicator X"},
 	{"1f1fe", "Regional Indicator Y"},
 	{"1f1ff", "Regional Indicator Z"},
-	-- Twemoji skips the fe0f
-	{"2a-20e3", "Keycap: *"},
-	{"23-20e3", "Keycap: #"},
-	{"30-20e3", "Keycap: 0"},
-	{"31-20e3", "Keycap: 1"},
-	{"32-20e3", "Keycap: 2"},
-	{"33-20e3", "Keycap: 3"},
-	{"34-20e3", "Keycap: 4"},
-	{"35-20e3", "Keycap: 5"},
-	{"36-20e3", "Keycap: 6"},
-	{"37-20e3", "Keycap: 7"},
-	{"38-20e3", "Keycap: 8"},
-	{"39-20e3", "Keycap: 9"},
 }
 for _, missing_item in ipairs(missing_codes) do
 	local code = missing_item[1]
@@ -123,7 +115,7 @@ end
 
 ordering = ordering.."}"
 
---#region Print the codes that didn't get added
+--#region Save the codes that didn't get added
 local missingCodes = io.open("./data-preprocessing/missing-codes.txt", "w")
 if not missingCodes then error("Can't write missing codes!") end
 for code in pairs(unregistered_emoji) do
