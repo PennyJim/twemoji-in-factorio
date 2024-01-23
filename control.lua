4800b17175ba58ac1702f10b6ff0fbcd181c5ebc
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-01-23 00:10:09 -0700

    Use on_configuration_changed

--- control.lua
+++ control.lua
@@ -10,29 +10,27 @@ local shortcodes = {
 local function register_shortcodes()
 	-- Register with better-chat if it was activated after twemoji
 	-- And mark it as unregistered if better-chat no longer exists
-	if script.active_mods["better-chat"] and not global["better-chat"] then
-		global["better-chat"] = true
+	if script.active_mods["better-chat"] then
 		remote.call("emojipack registration", "add", script.mod_name, shortcodes[
 			settings.global["shortcode-standard"].value
 		])
-	elseif not script.active_mods["better-chat"] and global["better-chat"] then
-		global["better-chat"] = nil
+	elseif not script.active_mods["better-chat"] then
+		shortcodes = nil
 	end
 end
 
 script.on_init(function ()
 	register_shortcodes()
 end)
-script.on_load(function ()
-	script.on_nth_tick(5, function ()
+script.on_configuration_changed(function (change_data)
+	local change = change_data.mod_changes["better-chat"]
+	if change then
 		register_shortcodes()
-		script.on_nth_tick(5, nil) -- Unregister
-	end)
+	end
 end)
 
 script.on_event(defines.events.on_runtime_mod_setting_changed ,function (change)
 	if change.setting == "shortcode-standard" then
-		global["better-chat"] = nil
 		register_shortcodes()
 	end
 end)
