19dbfd25172714d8bc15d4eea831a2073b2e8a9e
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-06-03 22:24:53 -0600

    Add a thumbnail

--- zipper.sh
+++ zipper.sh
@@ -3,4 +3,4 @@ if [ ! -f "./info.json" ]; then exit 1; fi
 mod_name=$(jq -r '.name' ./info.json)
 output_file=./${mod_name}_$(jq -r '.version' ./info.json).zip
 cd ../
-zip -r "${mod_name}/${output_file}" ${mod_name}/* -x "*/twemoji/*" "*/emojibase/*" "*/data-preprocessing/*" "*/assets/emoji-ordering.txt" "*/.git*" "*/zipper.sh"
+zip -r "${mod_name}/${output_file}" ${mod_name}/* -x "*/twemoji/*" "*/emojibase/*" "*/data-preprocessing/*" "*/assets/emoji-ordering.txt" "*/.git*" "*/*.xcf" "*/zipper.sh"
